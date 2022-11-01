fb7a5e384aba75ca552a6f5453519dc44548d5ce
antony ponson <antonyponson@gmail.com>
2022-11-01 18:34:28 +0530

    testmonial added

--- javascript/script.js
+++ javascript/script.js
@@ -34,3 +34,18 @@ const fadeElems = document.querySelectorAll('.has-fade');
 // mansory grid layout
 
 
+// swipper js
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
